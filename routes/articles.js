7111cd31aebc05ee8f6d2d10ba7b8d929468658a
Isaiah Harris <harrisfambam@gmail.com>
2018-07-26 16:12:35 -1000

    get works

--- routes/articles.js
+++ routes/articles.js
@@ -13,23 +13,23 @@ router.get('/', (req, res, next) => {
   })
 })
 
-router.post('/', (req, res, next)=>{
-  if(!req.body.title){
+router.post('/', (req, res, next) => {
+  if (!req.body.title) {
     res.send('no title')
     res.redirect('/articles/new')
-  }else if(!req.body.author){
+  } else if (!req.body.author) {
     res.send('no author')
     res.redirect('/articles/new')
   }else {
-      articlesData.add(req.body)
-      res.redirect('/articles');
+    articlesData.add(req.body)
+    res.redirect('/articles');
   }
 })
 
-router.put('/:title', (req,res,next)=>{
+router.put('/:title', (req, res, next) => {
   let title = req.params.title;
-  articlesData.all().map(element =>{
-    if(element.title === title){
+  articlesData.all().map(element => {
+    if (element.title === title) {
       element.title = req.body.title
     }
   })
@@ -38,12 +38,34 @@ router.put('/:title', (req,res,next)=>{
 
 router.delete(`/:title`, (req, res) => {
   let title = req.params.title;
-  for (let i = 0; i < articlesData.all().length; i++){
-    if (title === articlesData.all()[i].title){
+  for (let i = 0; i < articlesData.all().length; i++) {
+    if (title === articlesData.all()[i].title) {
       articlesData.remove(i);
     }
   }
   res.redirect('/articles');
+})
+/////////////
+
+
+/////////////
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    showArticles: true,
+    articles: articlesData.all()
+  })
+})
+
+router.get('/:title', (req, res) => {
+  let title = req.params.title;
+  articlesData.all().map(element => {
+    if (element.title === title) {
+      res.render('article',{
+         article: element
+      })
+    }
+  })
 })
 
 module.exports = router;
