98b1b2af28dd70f80432d906bc379ce16ff726a8
Isaiah Harris <harrisfambam@gmail.com>
2018-07-27 18:58:45 -1000

    finish products

--- db/productDb.js
+++ db/productDb.js
@@ -17,8 +17,57 @@ function addProduct (req){
   productCollection.push(newProduct);
 }
 
+function removeProduct(id, res){
+  let deletedProduct = false;
+  productCollection.map(element=>{
+    if(element.id === id){
+      let productIndex = productCollection.indexOf(element);
+      productCollection.splice(productIndex, 1)
+      deletedProduct = true;
+    }
+  })
+  if(deletedProduct === false){
+    deletedProduct = true;
+    res.redirect('products/new');
+  }else {
+    res.redirect('/products');
+  }
+}
+
+function findId(id) {
+  let elem;
+  productCollection.map(element => {
+    if (element.id === id) {
+      elem = element
+      getId= true
+    }
+  })
+  return elem;
+}
+
+function update(id, req, res){
+  productCollection.map(element =>{
+    if(element.id === id){
+      element.name = req.body.name;
+      element.price = req.body.price;
+      element.inventory = req.body.inventory;
+      element.id = req.body.id;
+      itemFound = true;
+    }
+  })
+  if(itemFound === false){
+    itemfound = true;
+    res.redirect(`/products/${title}/edit`)
+  }else {
+    res.redirect('/products');
+  }
+}
+
 module.exports = {
   allProducts,
   productIdGenerator,
-  addProduct
+  addProduct,
+  findId,
+  update,
+  removeProduct
 }
--- routes/products.js
+++ routes/products.js
@@ -6,11 +6,12 @@ const productData = require('../db/productDb');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get('/', (req,res)=>{
-  res.render('pIndex', {
+router.get('/', (req, res, next) => {
+  res.render('phome', {
     product: productData.allProducts()
-  });
+  })
 })
+
 
 router.post('/', (req,res)=>{
   if (!req.body.name || !req.body.price || !req.body.inventory) {
@@ -21,8 +22,47 @@ router.post('/', (req,res)=>{
   }
 })
 
+router.put('/:id', (req,res)=>{
+  productData.update(req.body.id, req, res);
+})
+
+router.delete('/:id', (req, res)=>{
+  productData.removeProduct(req.params.id, res)
+})
+
+router.get('/', (req,res)=>{
+  res.render('pindex', {
+    product: productData.allProducts()
+  })
+})
+
 router.get('/new', (req, res) => {
   res.render('pnew');
 })
+
+router.get('/:id', (req,res)=>{
+  let elem = productData.findId(req.params.id);
+  if(elem){
+    res.render('product', {
+      product:elem
+    })
+  }else {
+    res.render('pnew', {
+      product: req.params
+    })
+  }
+})
+
+router.get('/:id/edit', (req,res)=>{
+  let elem = productData.findId(req.params.id);
+  if(elem){
+    res.render('pedit', {
+      product: elem
+    })
+  }else {
+    res.render('pnew');
+  }
+})
+
 
 module.exports = router;
