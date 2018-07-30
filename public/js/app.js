c47611abf8fb6ddefde9ede9536b989b084d9369
Isaiah Harris <harrisfambam@gmail.com>
2018-07-29 18:12:16 -1000

    added styles to action

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,18 @@
 
-let articleBody = document.getElementById('article-body');
-articleBody.addEventListener('click', function(){
-  if(articleBody.id === 'article-body'){
-    articleBody.id = 'active'
-  }else if(articleBody.id === 'active'){
-    articleBody.id = 'article-body'
+let articleBody = document.getElementsByClassName('article-body');
+
+for (let i = 0; i < articleBody.length; i++) {
+  console.log(articleBody[i]);
+  articleBody[i].addEventListener('click', switchDaStuff);
+
+}
+function switchDaStuff() {
+
+    
+  
+  if (this.className === 'article-body') {
+    this.className = 'active'
+  } else if (this.className === 'active') {
+    this.className = 'article-body'
   }
-})
+}
