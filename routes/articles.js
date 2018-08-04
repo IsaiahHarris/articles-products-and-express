5bd664685c39fe2c979a783c661e3311564cd2cc
Isaiah Harris <harrisfambam@gmail.com>
2018-08-03 16:24:36 -1000

    art add

--- routes/articles.js
+++ routes/articles.js
@@ -2,4 +2,15 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/knex');
 
+
+
+router.get('/', (req,res)=>{
+  return db.select().from('articles')
+  .then(result=>{
+    res.render('home',{
+      article: result
+    })
+  })
+})
+
 module.exports = router;
