ab524ac7e1459b14c2e362e25eb839b94ae7d757
Isaiah Harris <harrisfambam@gmail.com>
2018-08-04 21:01:48 -1000

    solved migration issue

--- migrations/20180804160528_articles.js
+++ migrations/20180804160528_articles.js
@@ -1,15 +1,15 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("articles", (table)=>{
-    table.increments("id");
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("articles", (table) => {
+    table.increments();
     table.string("author").notNullable();
     table.text("body").notNullable();
     table.string("title").notNullable();
-    table.string("urlTitle");
+    table.string("urltitle");
     table.timestamps(true, true);
   })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTable("articles");
 };
